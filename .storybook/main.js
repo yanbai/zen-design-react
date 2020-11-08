a689a458ed8c130cf5813a52ca1003996c66a3e5
yanbai-kooboo <yanbai@kooboo.cn>
2020-11-09 07:51:22 +0800

    added mdx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
@@ -18,6 +20,39 @@ module.exports = {
         'assets': path.resolve(__dirname, '../src/assets')
       }
     }
+    config.module.rules.push({
+      test: /\.mdx$/,
+      exclude: [/node_modules/],
+      include: [
+        path.resolve(__dirname, '../src')
+      ],
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['@babel/plugin-transform-react-jsx']
+          }
+        },
+        {
+          loader: '@mdx-js/loader',
+          options: {
+            compilers: [createCompiler({})]
+            // remarkPlugins: [
+            //   [
+            //     codesandbox,
+            //     {
+            //       mode: 'iframe',
+            //       query: {
+            //         fontsize: 14
+            //       },
+            //       autoDeploy: true
+            //     }
+            //   ]
+            // ]
+          }
+        }
+      ]
+    })
     return console.dir(config, { depth: null }) || config
   }
 };
