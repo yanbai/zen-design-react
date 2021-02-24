dafa719c2c6ce9d8f5dfb81b194056ae3d6d600c
byne <byne.zhang@klook.com>
2021-02-24 11:22:27 +0800

    remove checkbox

--- src/components/AntCheckbox/index.js
+++
@@ -1,8 +0,0 @@
-// import AntCheckbox from "./checkbox"
-// import AntCheckboxGroup from "./group"
-import ACheckbox from "./ant-checkbox"
-import AntCheckboxGroup from "./ant-group"
-
-ACheckbox.Group = AntCheckboxGroup
-
-export default ACheckbox
